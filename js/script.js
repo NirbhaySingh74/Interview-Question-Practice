42cc00e1f90de47b21b4a334a8f95c2f9e4b460c
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-13 09:13:19 +0530

    type

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!!");
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+const person = {
+  name: "Nirbhay",
+  age: 21
+}
+const name = ['John','doe']
+
+console.log(typeof name);
+console.log(typeof person);
+console.log(typeof add);
+console.log(typeof Car);
