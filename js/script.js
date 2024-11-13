a64de400e96c3154831294625781a40464fa2eb6
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-13 09:45:05 +0530

    prmises

--- js/script.js
+++ js/script.js
@@ -11,11 +11,49 @@ function add(a, b) {
 
 const person = {
   name: "Nirbhay",
-  age: 21
-}
-const name = ['John','doe']
+  age: 21,
+};
+const name = ["John", "doe"];
 
-console.log(typeof name);
-console.log(typeof person);
-console.log(typeof add);
-console.log(typeof Car);
+// console.log(typeof name);
+// console.log(typeof person);
+// console.log(typeof add);
+// console.log(typeof Car);
+
+//         **          PROMISE IN JAVASCRIPT     **
+
+// const myPromise = new Promise((resolve, reject) => {
+//   const success = true;
+
+//   setTimeout(() => {
+//     if (success) {
+//       resolve("success");
+//     } else {
+//       reject("Fail");
+//     }
+//   }, 2000); // Waits for 2 seconds
+// });
+
+
+
+const btnElement = document.getElementById("btn");
+
+btnElement.addEventListener("click", () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const success = true;
+
+    setTimeout(() => {
+      if (success) {
+        resolve("success");
+      } else {
+        reject("Fail");
+      }
+    }, 2000); 
+  });
+
+  myPromise
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err))
+    .finally(() => console.log("settled"));
+});
+
