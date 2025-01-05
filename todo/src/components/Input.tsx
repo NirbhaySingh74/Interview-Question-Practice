12c8c5395f59ef3ff548bea5216f9723c7b65458
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-01-05 18:10:34 +0530

    Input components Designed

--- todo/src/components/Input.tsx
+++ todo/src/components/Input.tsx
@@ -1,8 +1,14 @@
 const Input = () => {
   return (
-    <div>
-      <input type="text" placeholder="Enter Text" />
-      <button>Add</button>
+    <div className="flex justify-center items-center p-4 gap-4  bg-gray-100 rounded-lg shadow-md">
+      <input
+        type="text"
+        placeholder="Enter Text"
+        className="w-64 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200">
+        Add
+      </button>
     </div>
   );
 };
